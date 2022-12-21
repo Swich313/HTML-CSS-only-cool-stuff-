c9456ffa9fbe089ae0974a78ce1609eb9870e18e
Swich <andruha313@gmail.com>
2022-12-21 15:31:12 +0200

    navigation and buttons with ripple effect added

--- article & hamburer/main.js
+++ article & hamburer/main.js
@@ -1,6 +1,7 @@
+//humburger
 const buttonTwo = document.querySelector('.button-two');
 const buttonThree = document.querySelector('.button-three');
-
+const textBox = document.querySelector('.box');
 
 buttonTwo.addEventListener('click', () => {
     const currentState = buttonTwo.getAttribute('data-state');
@@ -23,3 +24,47 @@ buttonThree.addEventListener('click', () => {
         buttonThree.setAttribute('aria-expanded', 'false');
     }
 });
+
+//text search
+const search = document.querySelector('#search');
+
+search.addEventListener('input', event => {
+   const searchText = event.target.value;
+   const reg = new RegExp(searchText, 'g');
+   let text = textBox.innerHTML;
+   text = text.replace(/(<mark class="highlighted">|<\/mark>)/gim, '');
+   textBox.innerHTML = text.replace(reg, '<mark class="highlighted">$&</mark>');
+});
+
+//navigation bar
+const list = document.querySelectorAll('.list');
+
+function activeLink(){
+    list.forEach(item => {
+        item.classList.remove('active');
+        this.classList.add('active');
+    });
+}
+
+list.forEach(item => {
+    item.addEventListener('click', activeLink);
+});
+
+
+//buttons with ripple effects
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(item => {
+    item.addEventListener('click', function(e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove();
+        }, 1000);
+    })
+})
